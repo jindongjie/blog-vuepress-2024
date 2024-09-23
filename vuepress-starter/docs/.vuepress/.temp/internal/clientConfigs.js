045fcf094bd916b4516109a5add5621f1ec9eb6c
ar0m <ar0m@tutanota.com>
2024-09-23 20:57:49 +0800

    Init

---
+++ vuepress-starter/docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,25 @@
+import * as clientConfig0 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@_2vtwzsob6ubb4nbf76g2t3iwpi/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc_remyvulcsmsgeg2ep2xazfhmna/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc.7_yiuz5ayvobntjywqngg72a7etu/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.47_markdown-it@14.1.0_vue@3.5.8_vuepress@2.0.0-rc.15__kgybejcmitrnhg6juy3sul4ad4/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc_mz7xw6m5udjd3c675kslne2vyy/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc.7_6odrkmhhsp3cslktykcyvfc7q4/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from '/home/Archf0ever/Documents/Blog/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from '/home/Archf0ever/Documents/Blog/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.47_vuepress@2.0.0-rc.15_@vuepress+bundler-vite@2.0.0-rc._cpoi2tx5zbghum2zywtqadnzmi/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from '/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.49_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+bundler_wolxj4uuncihoibzjeo536veny/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig10 from '/home/Archf0ever/Documents/Blog/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+].map((m) => m.default).filter(Boolean)
---
+++ vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import CodeTabs from "/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.47_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+b_qcfixsruljac274hi62oqmb3zq/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import Tabs from "/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.47_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+b_qcfixsruljac274hi62oqmb3zq/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "/home/Archf0ever/Documents/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.47_markdown-it@14.1.0_vuepress@2.0.0-rc.15_@vuepress+b_qcfixsruljac274hi62oqmb3zq/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
